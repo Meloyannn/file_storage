d97f79370b31507be8ada56329f537252e420577
Hovhannes Darbinyan <hdarbinyan@dlink.ru>
2019-09-05 12:30:53 +0400

    .

--- web/server.js
+++ web/server.js
@@ -1,18 +1,17 @@
 const express = require("express"),
-      bodyParser = require("body-parser"),
-      cookieParser = require("cookie-parser"),
-      path = require('path'),
-      dotenv = require('dotenv'),
-      request = require("request"),
-      app = express()
-      dotenv.config()
-      PORT = process.env.PORT,
-      API_URL = process.env.API_URL
-      
+    bodyParser = require("body-parser"),
+    cookieParser = require("cookie-parser"),
+    path = require('path'),
+    dotenv = require('dotenv'),
+    request = require("request"),
+    app = express()
+    dotenv.config()
+    PORT = process.env.PORT,
+    API_URL = process.env.API_URL
 
 app.use(cookieParser())
 app.use(bodyParser.json())
-console.log(path.join(__dirname, "build"))
+
 app.use(express.static(path.join(__dirname, "build")))
 app.get('/api/*',function (req, res) {
 	request(`${API_URL}${req.params[0]}`,{headers:req.headers}).on('error', err => res.sendStatus(500)).pipe(res)
